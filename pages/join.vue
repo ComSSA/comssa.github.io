87b6755c086a3ee19a9c2c3677f9b4819d09a5da
Anurag <anuragsingh3@outlook.com>
2021-02-27 17:43:46 +0800

    bootstrapify the join page

--- pages/join.vue
+++ pages/join.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="join">
-    <h2>Why should I join ComSSA?</h2>
+  <b-container fluid>
+    <h2 class="mt-3">
+      Why should I join ComSSA?
+    </h2>
     <ul>
       <li>Get discounts to ALL events.</li>
       <li>Gain access to the ComSSA common room (314.345) after hours.</li>
       <li>Get exclusive access to select events.</li>
       <li>Get free access to select events.</li>
-    </ul> <br>
+    </ul>
 
     <h2>Joining ComSSA</h2>
     To get the most out of ComSSA, you’ll want to become an official member. You’ll receive a membership sticker to place on
@@ -15,7 +17,7 @@
     with a Curtin Student Guild club membership voucher. Interested in joining? You can join in one of two ways: <br><br>
     Note: If you’re using a club membership voucher then you must follow the In-Person method.
 
-    <h2 id="subheading">
+    <h2 class="mt-3">
       In-Person
     </h2>
     Talk to a committee member around building 314 at Curtin and we’ll be happy to sign you up. You can often find us hanging
@@ -25,7 +27,7 @@
     the committee posters on one of the notice boards in 314 . Can’t find a committee member? Contact a committee member through
     discord and setup a time.
 
-    <h2 id="subheading">
+    <h2 class="mt-3">
       Online
     </h2>
     If you’d prefer to sign up online, that’s even easier! Unfortunately, you can’t use club membership vouchers to sign up online.
@@ -33,7 +35,7 @@
     To get started, head over <a href="https://comssamemberships.getqpay.com/">here</a>.
     You’ll be asked for your Curtin Student Guild membership status, so please use this to let us know if you are a Guild member.
     Please do not mark yourself as paying with a voucher — this will not lessen any online membership fees.
-  </div>
+  </b-container>
 </template>
 
 <script>
