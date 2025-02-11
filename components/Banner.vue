02b0ce5e2b75e2b2ab0fa37674630d8a0f5c1b39
Sean McGinty <newfolderlocation@gmail.com>
2025-02-11 15:44:38 +0000

    Banner for new website

---
+++ components/Banner.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-100" style="position: fixed; background: #4559A7; height: 20px; z-index: 1;">
+    <a href="https://new.comssa.org.au" style="color: white; text-align: center; display: block; padding: 2px 0; font-size: 12px; font-weight: 400;">
+      Click here to see our new website!</a>
+  </div>
+</template>
